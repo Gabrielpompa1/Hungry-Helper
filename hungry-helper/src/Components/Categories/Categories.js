04b9856e0b1905f0449ca300f3e55e260caac5cc
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-10-29 14:40:47 -0700

    Solid work. Each category will display all meals within itself. Next I need to change the url endpoint to match the category selected.

--- hungry-helper/src/Components/Categories/Categories.js
+++ hungry-helper/src/Components/Categories/Categories.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Category from '../Category/Category'
 
 const Categories = () => {
-	const [category, setCategories] = useState('');
+	const [category, setCategory] = useState('');
 
 	const apiKey = process.env.REACT_APP_STOCK_API_KEY;
 	// List all Categories
@@ -12,7 +12,8 @@ const Categories = () => {
 		fetch(catUrl)
 			.then((res) => res.json())
 			.then((resJson) => {
-				setCategories(resJson.categories);
+                setCategory(resJson.categories);
+                console.log(category)
 			})
 			.catch((error) => {
 				console.error(error);
@@ -21,26 +22,16 @@ const Categories = () => {
 
 	if (!category) {
 		return null;
-	}
+    }
+    let catList = category.map((item) => {
+        return (
+            <li key={item.idCategory}>{item.strCategory}</li>
+        )
+    })
 	return (
 		<div>
 			<h2>Choose a category to get started!</h2>
-			<ul>
-				<li>{category[0].strCategory}</li>
-				<li>{category[1].strCategory}</li>
-				<li>{category[2].strCategory}</li>
-				<li>{category[3].strCategory}</li>
-				<li>{category[4].strCategory}</li>
-				<li>{category[5].strCategory}</li>
-				<li>{category[6].strCategory}</li>
-				<li>{category[7].strCategory}</li>
-				<li>{category[8].strCategory}</li>
-				<li>{category[9].strCategory}</li>
-				<li>{category[10].strCategory}</li>
-				<li>{category[11].strCategory}</li>
-				<li>{category[12].strCategory}</li>
-				<li>{category[13].strCategory}</li>
-			</ul>
+            <ul>{catList}</ul>
             <Category />
 		</div>
 	);
--- hungry-helper/src/Components/Category/Category.js
+++ hungry-helper/src/Components/Category/Category.js
@@ -3,13 +3,14 @@ import React, { useState, useEffect } from 'react';
 const Category = () => {
 	const [listItem, setListItem] = useState('');
 	const apiKey = process.env.REACT_APP_STOCK_API_KEY;
-	const catListUrl = `https://www.themealdb.com/api/json/v1/${apiKey}/filter.php?c=Seafood`;
+	const catListUrl = `https://www.themealdb.com/api/json/v1/${apiKey}/filter.php?c=Beef`;
 
 	useEffect(() => {
 		fetch(catListUrl)
 			.then((res) => res.json())
 			.then((resJson) => {
 				setListItem(resJson.meals);
+				console.log(listItem);
 			})
 			.catch((error) => {
 				console.error(error);
@@ -20,11 +21,12 @@ const Category = () => {
 		return null;
 	}
 
+	let list = listItem.map((item) => {
+    return <li key={item.idMeal}>{item.strMeal}</li>;
+	});
 	return (
 		<div>
-			<ul>
-				<li>{listItem[0].strMeal}</li>
-			</ul>
+			<ul>{list}</ul>
 		</div>
 	);
 };
